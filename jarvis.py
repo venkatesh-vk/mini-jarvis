b49109cfc6ff7005716e4c86c79a500a0f807f68
venkatesh-vk <61019595+venkatesh-vk@users.noreply.github.com>
2021-05-22 12:20:32 +0530

    Update jarvis.py

--- jarvis.py
+++ jarvis.py
@@ -84,7 +84,7 @@ if __name__ == "__main__":
 
 
         elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
+            music_dir = 'Your music player location'
             songs = os.listdir(music_dir)
             print(songs)    
             os.startfile(os.path.join(music_dir, songs[0]))
@@ -94,7 +94,7 @@ if __name__ == "__main__":
             speak(f"Sir, the time is {strTime}")
 
         elif 'open code' in query:
-            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            codePath = "Your VS code location"
             os.startfile(codePath)
 
         elif 'email to harry' in query:
@@ -106,4 +106,4 @@ if __name__ == "__main__":
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend harry bhai. I am not able to send this email")    
+                speak("Sorry sir I am not able to send this email")    
